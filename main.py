c91cf8e2132a5d7806d728df4b94301f7fa72d2e
eoincowhey <eoincowhey@hotmail.com>
2021-03-21 22:04:29 +0000

    Temperature Calcs V1.

--- main.py
+++ main.py
@@ -46,11 +46,20 @@ Temp = Temp.drop(["OLI","OTI_A","OTI_T","MOG_A"], axis = "columns")
 
 # Power Calculations
 
-# Merging Current/Voltage Data Frame with Power Factor which is required for Power Calculations
-Power_Data_Raw = I_V.merge(PF, on="DeviceTimeStamp", how="left")
+# Merging Current/Voltage Data Frame with Power Factor which is required for Power Calculations.
+Power_Data = I_V.merge(PF, on="DeviceTimeStamp", how="left")
+# Merging Temperature Data Frame with newly created Power_Data.
+Power_Data = Power_Data.merge(Temp, on="DeviceTimeStamp", how="left")
 
 # Where time stamps do not line up following left merge, missing Power Factor values are assigned 1 or unity power factor.
-Power_Data = Power_Data_Raw.fillna(1)
+values = {'PFL1': 1, 'PFL2': 1, 'PFL3': 1, 'FRQ': 50}
+Power_Data[['PFL1','PFL2','PFL3','FRQ']] = Power_Data[['PFL1','PFL2','PFL3','FRQ']].fillna(value=values)
+# For OTI as temperature change is normally slow and predictable, therefore the missing values are forward filled.
+Power_Data[['OTI','WTI','ATI']] = Power_Data[['OTI','WTI','ATI']].fillna(method='ffill')
+
+Missing_Power_Data = Power_Data.isnull().sum()
+print(Missing_Power_Data)
+
 
 # To demonstrate the use of functions where the same calculation may be required multiple times.
 # Functions are defined for Active and Reactive Power calculations.
@@ -78,7 +87,7 @@ kW_total = []
 kvar_total = []
 
 for i, row in Power_Data.iterrows():
-   kW = (row["WL1"] + row["WL2"] + row["WL3"])/1000
+   kW = (row["WL1"] + row["WL2"] + row["WL3"]) / 1000
    kvar = (row["varL1"] + row["varL2"] + row["varL3"]) / 1000
    kW_total.append(kW)
    kvar_total.append(kvar)
@@ -90,21 +99,60 @@ Power_Data["kvar_total"] = kvar_total
 Power_Data["kVA_total"] = (Power_Data["kW_total"]**2 + Power_Data["kvar_total"]**2)**(1/2)
 
 
-#Subsetting Rows by Date/Time Stamp Range (2019-06-25T13:06 to 2020-04-14T00:30)
+#Date/Time Stamp Range (2019-06-25T13:06 to 2020-04-14T00:30)
+# Creating new column in data frame to assign Month/Year and Month/Year/Day for each sample row.
 Power_Data['Month_Year'] = Power_Data['DeviceTimeStamp'].dt.to_period('M')
-Power_Data['Days'] = Power_Data['DeviceTimeStamp'].dt.to_period('D')
+Power_Data['Month_Year_Day'] = Power_Data['DeviceTimeStamp'].dt.to_period('D')
 
 # Removing indexes and setting DeviceTimeStamp as the Index.
 Power_Data = Power_Data.set_index("DeviceTimeStamp")
 
+# Using Numpy to extract summary statistic data from Power Data for each Month
+Power_stats = Power_Data.groupby("Month_Year")[["kW_total","kvar_total"]].agg([np.min,np.max, np.mean])
+
 print(Power_Data.head())
 print(Power_Data.shape)
+print(Power_stats)
+
+
+# Temperature Calculations
+
+Temperature_stats = Power_Data.groupby("Month_Year")[["OTI","WTI","ATI"]].agg([np.min, np.max, np.mean])
+print(Temperature_stats)
+
+
+# Average Ambient Temperature, Oil Temperature and Current (15 minute intervals)
+range = pd.date_range('2019-06-25', '2020-04-14', freq='15min')
+Temperature_Data = pd.DataFrame(index = range)
+Temperature_Data['OTI'] = np.random.randint(low=0, high=50, size=len(Temperature_Data.index))
+Temperature_Data['IL2'] = np.random.randint(low=0, high=50, size=len(Temperature_Data.index))
+Temperature_Data['ATI'] = np.random.randint(low=0, high=50, size=len(Temperature_Data.index))
+
+print(Temperature_Data.head(100))
+
+def CT_Calc(Current, CT_Primary, CT_Secondary):
+    return (Current/CT_Primary)*CT_Secondary
+
+Temperature_Data['I_sec'] = CT_Calc(Temperature_Data['IL2'],300,1.5)
+
+Temp_Sensor = pd.read_csv('Temp_Sensor.csv')
+
+def lookup_grade(I_ret):
+    match = Temp_Sensor['Current'] <= I_ret
+    grade = Temp_Sensor['Temp'][match]
+    return grade.values[0]
+
+Temperature_Data['I_sec'].apply(lookup_grade)
+
+print(Temperature_Data.head())
 
 
 # Plotting Charts
 #Calling additional packages to be imported for plotting charts.
 import seaborn as sns
 import matplotlib.pyplot as plt
+
+#July_2019 = Power_Data.loc["2019-09-01":"2019-09-02"]
 
 # Set the style to "darkgrid"
 sns.set_style("darkgrid")
@@ -118,10 +166,12 @@ plt.xticks(rotation=0)
 # Set title to "Age of Those Interested in Pets vs. Not"
 g.fig.suptitle("kW vs. Months", y=1)
 
-
 # Add x-axis and y-axis labels
 g.set(xlabel="Active Power [kW]", ylabel="Month")
 
+
+
+plt.show()
 
 
 #Add a title "Average MPG Over Time"
@@ -134,7 +184,7 @@ g.set(xlabel="Active Power [kW]", ylabel="Month")
       #ylabel="% Who Like Techno")
 
 # Show plot
-plt.show()
+#plt.show()
 
 
 
